3074e32772bb5fbbed86fceda67b36f2410f6458
marcusziade <marcus.ziade@ouraring.com>
2023-04-13 21:54:18 +0300

    Add PokeAPIClientTests with testFetchPokemon and testFetchNonExistentPokemon methods
    Closes #8

--- Tests/SwiftPokeKitTests/PokeAPIClientTests.swift
+++ Tests/SwiftPokeKitTests/PokeAPIClientTests.swift
@@ -4,11 +4,12 @@ import XCTest
 
 @available(iOS 15, *)
 @available(macOS 12, *)
-final class APIExtensionsTests: XCTestCase {
+final class PokeAPIClientTests: XCTestCase {
+    var sut: PokeAPIClient!
     
     override func setUp() {
         super.setUp()
-        sut = URLSession.shared
+        sut = PokeAPIClient()
     }
     
     override func tearDown() {
@@ -16,29 +17,21 @@ final class APIExtensionsTests: XCTestCase {
         super.tearDown()
     }
     
-    func testSuccessfulRequest() async {
-        let request = PokemonAPIRequest(name: "pikachu")
-        
+    func testFetchPokemon() async throws {
         do {
-            let pokemon = try await sut.request(request)
-            XCTAssertEqual(pokemon.name, "pikachu")
-            XCTAssertNotNil(pokemon.abilities)
-            XCTAssertNotNil(pokemon.types)
+            let fetchedPokemon = try await sut.fetchPokemon(name: "pikachu")
+            XCTAssertEqual(fetchedPokemon.name, "pikachu")
         } catch {
-            XCTFail("Error: \(error)")
-        }
-    }
-
-    func testFailedRequest() async {
-        let request = PokemonAPIRequest(name: "invalid-pokemon-name")
-        
-        do {
-            let _ = try await sut.request(request)
-            XCTFail("Expected an error, but the test succeeded")
-        } catch {
-            // Test passes if an error is thrown, as expected.
+            XCTFail("Failed to fetch Pikachu: \(error)")
         }
     }
     
-    var sut: URLSession!
+    func testFetchNonExistentPokemon() async throws {
+        do {
+            _ = try await sut.fetchPokemon(name: "nonexistentpokemon")
+            XCTFail("Expected an error, but the test succeeded")
+        } catch {
+            // Test passes if an error is thrown for a non-existent Pokémon
+        }
+    }
 }
